afd7601e0a2f8a31114ab6ddb73807b2a25b4741
KevinGilpin <kevin.gilpin@gmail.com>
2018-04-13 01:52:08 -0700

    login responsive redesign

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ module.exports = function(passport){
       return res.redirect(req.user.company);
     } else {
       req.session.destroy(function() {
-        return res.render("login", { layout: null });
+        return res.render("signin", { layout: null });
       });
     }
   });
